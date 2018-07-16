fe48360547d5db8a37ee3eb698933c1dc176f9b4
Kaden Thomas <thomas.kaden4@outlook.com>
2018-07-16 13:47:38 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # babel-node-pp
-An upgraded version of babel-node that uses .babelrc
+An upgraded version of babel-node that uses .babelrc instead of needing arguments passed via arguments.
+
+### Usage
+
+Same as `babel-node`, except that `preset`s found in `.babelrc` will be appended to any passed via the command line.
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { spawn } from "cross-spawn";
+import { resolve } from "path";
+import fs, { access } from "fs";
+import minimist from "minimist";
+import { inherits } from "util";
+
+const args = minimist(process.argv.slice(2));
+
+// TODO handle paths
+const bnode_exe = "node_modules/.bin/babel-node";
+
+// TODO handle read errors
+const babelrc_data = JSON.parse(fs.readFileSync('.babelrc'));
+
+// "foo,bar" -> "baz" = "foo,bar,baz"
+const mergeArgumentLists = (_given, extra) => {
+    const given = _given ? [_given] : [];
+    return [...given, ...extra].join(',');
+};
+
+// merge given arguments with the arguments in .babelrc
+const presets = mergeArgumentLists(args.presets, babelrc_data.presets);
+const plugins = mergeArgumentLists(args.plugins, babelrc_data.plugins);
+
+const long_args = [
+    "--presets", presets,
+    "--plugins", plugins
+];
+
+// get final args (babel-node will ignore repeated args, taking the second instance)
+const final_args = [...process.argv.slice(2), ...long_args];
+
+// Forward arguments to babel-node
+let child = spawn(bnode_exe, final_args, { stdio: 'inherit' });
+child.on('error', e => {
+    console.error("babel-node-pp: unable to spawn child process");
+});
